837c018a5185224000724450cd7e94a6ff5db847
Rasmus Nordling <rasmus.nordling@gmail.com>
2016-12-30 11:26:26 +0100

    Add methods for getting information about a person

--- PersonDirectory/PersonHandler.cs
+++ PersonDirectory/PersonHandler.cs
@@ -4,12 +4,6 @@ namespace PersonDirectory
 {
     class PersonHandler
     {
-        public void SetAge(Person pers, int age)
-        {
-            if (pers != null)
-                pers.Age = age;
-        }
-
         public Person CreatePerson(int age, string fname, string lname,
                                     double height, double weight)
         {
@@ -21,6 +15,36 @@ namespace PersonDirectory
                 Height = height,
                 Weight = weight
             };
+        }
+
+        public void SetAge(Person pers, int age)
+        {
+            if (pers != null)
+                pers.Age = age;
+        }
+
+        public string FullName(Person person)
+        {
+            if (person == null)
+                return "";
+
+            return person.LastName + ", " + person.FirstName; 
+        }
+
+        public int CalculateBirthYear(Person person)
+        {
+            if (person == null)
+                return 0;
+
+            return DateTime.Now.Year - person.Age;
+        }
+
+        public double CalculateBMI(Person person)
+        {
+            if (person == null)
+                return 0;
+
+            return person.Weight / (person.Height * person.Height);
         }
     }
 }
--- PersonDirectory/Program.cs
+++ PersonDirectory/Program.cs
@@ -6,20 +6,35 @@ namespace PersonDirectory
     {
         static void Main(string[] args)
         {
-            var person = new Person()
-            {
-                Age = 26,
-                FirstName = "John",
-                LastName = "Doe",
-                Height = 175,
-                Weight = 75
-            };
+            //var person = new Person()
+            //{
+            //    Age = 26,
+            //    FirstName = "John",
+            //    LastName = "Doe",
+            //    Height = 175,
+            //    Weight = 75
+            //};
 
-            Console.WriteLine($"Age: {person.Age}");
-            Console.WriteLine($"Firstname: {person.FirstName}");
-            Console.WriteLine($"Lastname: {person.LastName}");
-            Console.WriteLine($"Height: {person.Height}");
-            Console.WriteLine($"Weight: {person.Weight}");
+            var handler = new PersonHandler();
+            var person = handler.CreatePerson(0, "John", "Doe", 1.75, 75);
+
+            PrintPerson(person);
+            Console.WriteLine();
+
+            handler.SetAge(person, 26);
+            Console.WriteLine($"{handler?.FullName(person)}");
+            Console.WriteLine($"is {person?.Age} years of age");
+            Console.WriteLine($"and was born in {handler?.CalculateBirthYear(person)}");
+            Console.WriteLine($"and has a BMI of: {handler?.CalculateBMI(person)}");
+        }
+
+        private static void PrintPerson(Person person)
+        {
+            Console.WriteLine($"Age: {person?.Age}");
+            Console.WriteLine($"Firstname: {person?.FirstName}");
+            Console.WriteLine($"Lastname: {person?.LastName}");
+            Console.WriteLine($"Height: {person?.Height}");
+            Console.WriteLine($"Weight: {person?.Weight}");
         }
     }
 }
